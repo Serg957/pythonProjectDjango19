35f3ee71fd572397275a79ac1cc7e9c5a9647b0b
Сергей <sh.s.e.57@gmail.com>
2025-01-07 19:01:58 +0300

    Направляю модуль 19.3

--- UrbanDjango_DB/UrbanDjango_DB/urls.py
+++ UrbanDjango_DB/UrbanDjango_DB/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+
 from django.contrib import admin
 from django.urls import path
+from task1.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
+    path('game/', game_page),
+    path('cart/', cart_page),
+    path('sing/', sign_up_by_django),
 ]
--- UrbanDjango_DB/task1/models.py
+++ UrbanDjango_DB/task1/models.py
@@ -1,22 +1,44 @@
+# from django.db import models
+#
+# # Create your models here.
+#
+# class Buyer(models.Model):
+#     name = models.CharField(max_length=100)
+#     balance = models.DecimalField(max_digits=10, decimal_places=2)
+#     age = models.IntegerField()
+#
+#     def __str__(self):
+#         return self.name
+#
+# class Game(models.Model):
+#     title = models.CharField(max_length=200)
+#     cost = models.DecimalField(max_digits=10, decimal_places=2)
+#     size = models.DecimalField(max_digits=10, decimal_places=2)
+#     description = models.TextField()
+#     age_limited = models.BooleanField(default=False)
+#     buyer = models.ManyToManyField(Buyer, related_name='games')
+#
+#     def __str__(self):
+#         return self.title
 from django.db import models
 
-# Create your models here.
 
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.DecimalField(decimal_places=2, max_digits=10)
     age = models.IntegerField()
 
     def __str__(self):
         return self.name
 
+
 class Game(models.Model):
-    title = models.CharField(max_length=200)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    size = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(decimal_places=2, max_digits=100)
+    size = models.DecimalField(decimal_places=2, max_digits=100)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer, related_name='game')
+    buyer = models.ManyToManyField(Buyer, related_name='buyers')
 
     def __str__(self):
         return self.title
